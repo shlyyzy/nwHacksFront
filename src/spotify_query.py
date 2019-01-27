8119fcd3780e179125147d601a62b0e31af9b66a
Greyson Wang <greysonw@sfu.ca>
2019-01-27 11:52:25 -0800

    server workssssss

--- src/spotify_query.py
+++ src/spotify_query.py
@@ -17,28 +17,36 @@ thisdict = {
 }
 
 
+@app.route('/data')
+def data():
+    # here we want to get the value of user (i.e. ?user=some-value)
+    time = request.args.get('time')   
+    
+
 @app.route('/')
 def hello_world():
     return 'this is /'
 
 
-@app.route('/getArtist/<name>', methods=['GET', 'POST'])
-def test(name):
-    print("printing this name via a pram from incoming URL: " + name)
-    return jsonify(thisdict)
+@app.route('/<type>/<mins>', methods=['GET', 'POST'])
+def test(type,mins):
+    print(type)
+    print(mins)
+    
+    id = updateSpotify(type, mins)
+    
+    return id
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
-
+def updateSpotify(selected_playlist, mins):
     username = "ozujzpuiqgx8oo3k0kzj4cnk1"
     token = util.prompt_for_user_token(username, scope='playlist-modify-private,playlist-modify-public', client_id='8ec10118123745c98796a0da389e1b35', client_secret='832c43bccfe44e6d90cd3ac66cc7e28b', redirect_uri='https://localhost:8080')
 
-    selected_playlist = 'workout'
     track_list = list()
     track_duration = list() # in ms
     new_tracks = list()
     user_playlistDict = {}
+    track_id = list()
 
     productive = ['37i9dQZF1DX1OY2Lp0bIPp', '37i9dQZF1DWUvQoIOFMFUT']
     workout = ['37i9dQZF1DX76Wlfdnj7AP', '37i9dQZF1DXdxcBWuJkbcy']
@@ -54,7 +62,7 @@ if __name__ == '__main__':
     else:
         sel_playlist = nostalgic
 
-    max_time = 1786140
+    max_time = int(mins) * 1000 * 60
     sum = 0
 
     if token:
@@ -62,12 +70,14 @@ if __name__ == '__main__':
         
         # track to get stuff from
         track1 = sp.user_playlist_tracks("spotify", playlist_id=sel_playlist[0])
+        print(track1)
+        print('max time:')
+        print(max_time)
         
         # Get my playlists.  
         my_playlists = sp.user_playlists(username)
         for i, my_playlists in enumerate(my_playlists['items']):
             user_playlistDict[my_playlists['name']] = my_playlists['id']
-        
         # If selected playlist doesnt exist, create it and add it to dict
         if user_playlistDict.get(selected_playlist) == None:
             print(user_playlistDict)
@@ -89,7 +99,14 @@ if __name__ == '__main__':
             if sum + track_duration[i] < max_time:
                 sum = sum + track_duration[i]
                 new_tracks.append(track_id[i])
-                sp.user_playlist_replace_tracks(username, playlist_id, new_tracks)
+                x = sp.user_playlist_replace_tracks(username, playlist_id, new_tracks)
+                print(x)
+                
+        return playlist_id
+
+    
+if __name__ == '__main__':
+    
 
         
-       # print(playlists)
+    app.run(host='0.0.0.0',port=8080)
